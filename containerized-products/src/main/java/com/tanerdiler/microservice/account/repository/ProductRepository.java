4d186dccb7a84fc98cde873199bbf49a4f8757df
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-28 11:58:07 +0300

    sonarLint Issues fixed & refactored

--- containerized-products/src/main/java/com/tanerdiler/microservice/account/repository/ProductRepository.java
+++ containerized-products/src/main/java/com/tanerdiler/microservice/account/repository/ProductRepository.java
@@ -9,23 +9,21 @@ import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository
-{
-	private final static Map<Integer, Product> products = new HashMap<>();
+public class ProductRepository {
+    private static final Map<Integer, Product> products = new HashMap<>();
 
-	static {
-		products.put(1, new Product(1, "5 saatlik şarj aleti", 100d));
-		products.put(2, new Product(2, "traş makinası", 5d));
-		products.put(3, new Product(3, "klavye", 200d));
-		products.put(4, new Product(4, "sırt çantası", 50d));
-	}
+    static {
+        products.put(1, new Product(1, "5 saatlik şarj aleti", 100d));
+        products.put(2, new Product(2, "traş makinası", 5d));
+        products.put(3, new Product(3, "klavye", 200d));
+        products.put(4, new Product(4, "sırt çantası", 50d));
+    }
 
-	public Optional<Product> findById(Integer id)
-	{
-		return Optional.ofNullable(products.get(id));
-	}
+    public Optional<Product> findById(Integer id) {
+        return Optional.ofNullable(products.get(id));
+    }
 
-	public Optional<Collection<Product>> findAll() {
-		return Optional.ofNullable(products.values());
-	}
+    public Optional<Collection<Product>> findAll() {
+        return Optional.of(products.values());
+    }
 }
--- containerized-products/src/main/java/com/tanerdiler/microservice/account/resource/ProductResource.java
+++ containerized-products/src/main/java/com/tanerdiler/microservice/account/resource/ProductResource.java
@@ -1,32 +1,29 @@
 package com.tanerdiler.microservice.account.resource;
 
+import com.tanerdiler.microservice.account.exception.ProductNotFoundException;
 import com.tanerdiler.microservice.account.model.Product;
 import com.tanerdiler.microservice.account.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/v1/products")
-public class ProductResource
-{
-	@Autowired
-	private ProductRepository repository;
+public class ProductResource {
+    @Autowired
+    private ProductRepository repository;
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Product> get(@PathVariable("id") Integer id)
-	{
-		return ResponseEntity.ok(repository.findById(id).get());
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> get(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(repository.findById(id).orElseThrow(() -> new ProductNotFoundException(id)));
+    }
 
-	@GetMapping()
-	public ResponseEntity<Collection<Product>> getAll()
-	{
-		return ResponseEntity.ok(repository.findAll().get());
-	}
+    @GetMapping("all")
+    public ResponseEntity<Collection<Product>> getAll() {
+        return ResponseEntity.ok(repository.findAll().orElse(new ArrayList<>()));
+    }
 }
