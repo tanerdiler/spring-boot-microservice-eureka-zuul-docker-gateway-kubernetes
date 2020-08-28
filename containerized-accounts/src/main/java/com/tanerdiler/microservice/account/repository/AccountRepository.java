a2fb45fb5ef6b50578d292f37d9725158b6e2cd4
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-28 11:40:31 +0300

    Refactoring & sonarLint issues fixed

--- containerized-accounts/src/main/java/com/tanerdiler/microservice/account/repository/AccountRepository.java
+++ containerized-accounts/src/main/java/com/tanerdiler/microservice/account/repository/AccountRepository.java
@@ -9,23 +9,22 @@ import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public class AccountRepository
-{
-	private final static Map<Integer, Account> accounts = new HashMap<>();
+public class AccountRepository {
 
-	static {
-		accounts.put(1, new Account(1, "Taner Diler", "taner.diler@gmail.com"));
-		accounts.put(2, new Account(2, "John Doe", "john.doe@mail.com"));
-		accounts.put(3, new Account(3, "Michael Knight", "michael.knight@mail.com"));
-		accounts.put(4, new Account(4, "Black Hawk", "black.hawk@mail.com"));
-	}
+    private static final Map<Integer, Account> accounts = new HashMap<>();
 
-	public Optional<Account> findById(Integer id)
-	{
-		return Optional.ofNullable(accounts.get(id));
-	}
+    static {
+        accounts.put(1, new Account(1, "Taner Diler", "taner.diler@gmail.com"));
+        accounts.put(2, new Account(2, "John Doe", "john.doe@mail.com"));
+        accounts.put(3, new Account(3, "Michael Knight", "michael.knight@mail.com"));
+        accounts.put(4, new Account(4, "Black Hawk", "black.hawk@mail.com"));
+    }
+
+    public Optional<Account> findById(Integer id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
 
     public Optional<Collection<Account>> findAll() {
-		return Optional.ofNullable(accounts.values());
+        return Optional.of(accounts.values());
     }
 }
