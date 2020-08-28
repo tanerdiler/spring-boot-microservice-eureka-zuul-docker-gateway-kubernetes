ef2f1f5cb30313e51f474c1544d30a2ef34537a8
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-28 11:53:14 +0300

    refactoring

--- containerized-main/src/main/java/com/tanerdiler/microservice/account/resource/BackofficeController.java
+++ containerized-main/src/main/java/com/tanerdiler/microservice/account/resource/BackofficeController.java
@@ -12,64 +12,59 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/backoffice")
-public class BackofficeController
-{
-	private static final Logger LOG = LoggerFactory.getLogger(BackofficeController.class);
+public class BackofficeController {
 
-	@Autowired
-	private ProductServiceClient productService;
-	@Autowired
-	private OrderServiceClient orderService;
-	@Autowired
-	private AccountServiceClient accountService;
+    private static final Logger LOG = LoggerFactory.getLogger(BackofficeController.class);
 
-	@GetMapping("/orders")
-	public ResponseEntity<List<OrderDTO>> getOrders()
-	{
+    @Autowired
+    private ProductServiceClient productService;
+    @Autowired
+    private OrderServiceClient orderService;
+    @Autowired
+    private AccountServiceClient accountService;
 
-		LOG.warn("Fetching all orders...");
-		List<Order> orders = orderService.findAll();
-		Map<Integer, Account> accounts = new HashMap<>();
-		Map<Integer, Product> products = new HashMap<>();
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderDTO>> getOrders() {
 
-		LOG.warn("Fetching accounts of orders...");
-		orders.stream()
-				.filter(o->!accounts.containsKey(o.getAccountId()))
-				.map(o->accountService.findById(o.getAccountId()))
-				.forEach(a->accounts.put(a.getId(), a));
+        LOG.warn("Fetching all orders...");
+        List<Order> orders = orderService.findAll();
+        Map<Integer, Account> accounts = new HashMap<>();
+        Map<Integer, Product> products = new HashMap<>();
 
-		LOG.warn("Fetching products of orders...");
-		orders.stream()
-				.filter(o->!products.containsKey(o.getProductId()))
-				.map(o->productService.findById(o.getProductId()))
-				.forEach(a->products.put(a.getId(), a));
+        LOG.warn("Fetching accounts of orders...");
+        orders.stream()
+                .filter(o -> !accounts.containsKey(o.getAccountId()))
+                .map(o -> accountService.findById(o.getAccountId()))
+                .forEach(a -> accounts.put(a.getId(), a));
 
-		LOG.warn("Generating composite of orders...");
-		List<OrderDTO> orderDTOList = new ArrayList<>();
-		orders.forEach(o->{
-			orderDTOList.add(new OrderDTO(
-					o.getId(),
-					o.getCount(),
-					o.getPrice(),
-					o.getDiscountedPrice(),
-					accounts.get(o.getAccountId()).getFullname(),
-					products.get(o.getProductId()).getName()
-			));
-		});
+        LOG.warn("Fetching products of orders...");
+        orders.stream()
+                .filter(o -> !products.containsKey(o.getProductId()))
+                .map(o -> productService.findById(o.getProductId()))
+                .forEach(a -> products.put(a.getId(), a));
 
-		return ResponseEntity.ok(orderDTOList);
+        LOG.warn("Generating composite of orders...");
+        List<OrderDTO> orderDTOList = new ArrayList<>();
+        orders.forEach(o -> {
+            orderDTOList.add(new OrderDTO(
+                    o.getId(),
+                    o.getCount(),
+                    o.getPrice(),
+                    o.getDiscountedPrice(),
+                    accounts.get(o.getAccountId()).getFullname(),
+                    products.get(o.getProductId()).getName()
+            ));
+        });
 
-	}
+        return ResponseEntity.ok(orderDTOList);
+
+    }
 }
