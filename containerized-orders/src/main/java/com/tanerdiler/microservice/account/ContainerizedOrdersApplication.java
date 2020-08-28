266f0f9f3494365c52155208c77d3b94c5bc6f5d
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-28 14:20:46 +0300

    renamed conflicted applications

--- containerized-orders/src/main/java/com/tanerdiler/microservice/account/ContainerizedOrdersApplication.java
+++ containerized-orders/src/main/java/com/tanerdiler/microservice/account/ContainerizedOrdersApplication.java
@@ -3,13 +3,12 @@ package com.tanerdiler.microservice.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ContainerizedAccountsApplication {
+public class ContainerizedOrdersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContainerizedAccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerizedOrdersApplication.class, args);
+    }
 }
--- containerized-products/src/main/java/com/tanerdiler/microservice/account/ContainerizedProductsApplication.java
+++ containerized-products/src/main/java/com/tanerdiler/microservice/account/ContainerizedProductsApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ContainerizedAccountsApplication {
+public class ContainerizedProductsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContainerizedAccountsApplication.class, args);
+        SpringApplication.run(ContainerizedProductsApplication.class, args);
     }
 }
